232f079353d9be5d0395fc654f69e5f5a863fd09
tan <nttrongtan1605@gmail.com>
2020-08-07 11:24:15 -0400

    Updates

---
+++ precache-manifest.094298c3d17f76bd6a4f4ec9b3451ae3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d0b5bdf3327709740ebe4ec6afd2c42",
+    "url": "/RutgersMenu/index.html"
+  },
+  {
+    "revision": "2e3f27d0c765de9e99c5",
+    "url": "/RutgersMenu/static/css/main.7a7a6ab1.chunk.css"
+  },
+  {
+    "revision": "7caf2287b6133139d910",
+    "url": "/RutgersMenu/static/js/2.8850500f.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/RutgersMenu/static/js/2.8850500f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e3f27d0c765de9e99c5",
+    "url": "/RutgersMenu/static/js/main.3e9ea697.chunk.js"
+  },
+  {
+    "revision": "6543a516acfc157a9494",
+    "url": "/RutgersMenu/static/js/runtime-main.0845cdfd.js"
+  }
+]);
